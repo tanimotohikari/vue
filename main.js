6b2a24d47b13c3d59f8d880621aca6d286cfcd72
tanimotohikari <tanimoto.hikari@gmail.com>
2019-04-05 05:29:30 +0900

    update:feat3 データバインディング

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
+var state = { count: 0 }
+
 var app = new Vue({
   el: '#app',
   data: {
+    /*
+      データ直下のプロパティは後から追加できないため
+      初期値やからデータとして定時しておく必要がある
+    */
+    /*
+      dataオプションに文字列やオブジェクトを定義することで,
+      インスタンス作成時にリアクティブなデータに変換される
+    */
     message: 'Hello Vue.js!',
     list: ['りんご', 'ばなな', 'いちご'],
     show: true,
-    show2: true
+    show2: true,
+    /*
+      オプション外で定義されたデータも
+      Vue.jsのdataとして登録するとリアクティブデータに変換される
+    */
+    state: state,
+    message2: {
+      value: 'Vue.js!',
+    },
+    num: 1
   },
   created: function() {
     // このインスタンスの作成&初期化が終わったらすぐ実行
